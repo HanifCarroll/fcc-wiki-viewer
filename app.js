eb0dc796917c5ccb05bd2e69e4a7cdd079f7439f
Hanif Carroll <HCarrollUSF@gmail.com>
2017-08-12 19:31:29 -0400

    Updated link

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function search (query) {
 	const URI = encodeURIComponent(query);
-	const url = `https://crossorigin.me/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${URI}&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max`;
+	const url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${URI}&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max`;
 	let data = await fetch(url).then((data) => data.json());
 	data = data.query.pages;
 	parseData(data);
@@ -17,17 +17,17 @@ function parseData(data) {
 	results.innerHTML = '';
 
 	for (element in data) {
-		
+
 		const a = document.createElement('a');
 		const div = document.createElement('div');
 		const h4 = document.createElement('h4');
 		const p = document.createElement('p');
-		
+
 		const name = data[element]['title'];
 		const desc = data[element]['extract'];
 		const pageid = data[element]['pageid'];
 		const link = `https://en.wikipedia.org/?curid=${pageid}`;
-		
+
 		div.classList.add('result');
 		results.appendChild(a);
 
